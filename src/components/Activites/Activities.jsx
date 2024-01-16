fba4b8e7299fddd5863b85c33664f9c3cce9a74f
Satyam <satyamjha1607@gmail.com>
2024-01-16 16:37:21 +0530

    updated content

--- src/components/Activites/Activities.jsx
+++ src/components/Activites/Activities.jsx
@@ -5,13 +5,13 @@ function Activities() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <Card
-        imagelink={"https://i.ytimg.com/vi/84ITUmPZz1A/maxresdefault.jpg "}
-        text={"activity1"}
+        imagelink={"https://c.ndtvimg.com/2021-01/fg4o5d6g_republic-day-2021-republic-day-wishes_625x300_25_January_21.jpg"}
+        text={"We, the EBSB Club at GGSIPU EDC is excited to announce our participation in the Republic Day parade on 26th January. We believe it's a fantastic opportunity for our club members to come together and celebrate the spirit of our nation."}
         size={"w-full md:w-96"}
-        outt={"https://ekbharat.gov.in/Home/Index"}
-        act={"click me"}
+        outt={"/home"}
+        act={""}
       />
-      <Card
+      {/* <Card
         imagelink={"https://i.ytimg.com/vi/84ITUmPZz1A/maxresdefault.jpg "}
         text={"activity2"}
         size={"w-full md:w-96"}
@@ -59,7 +59,7 @@ function Activities() {
         size={"w-full md:w-96"}
         outt={"https://ekbharat.gov.in/Home/Index"}
         act={"click me"}
-      />
+      /> */}
     </div>
   );
 }
