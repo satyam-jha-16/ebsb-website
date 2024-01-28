63e8480eb45ec6208e3aa7549bd3db51c4ebf2b2
Karan Bhatia <karan.01819011722@ipu.ac.in>
2024-01-28 17:39:02 +0530

    Added Framer Motion Animations and some minor changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <div>
+    <div >
       <footer className="bg-white border-y flex justify-end">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
